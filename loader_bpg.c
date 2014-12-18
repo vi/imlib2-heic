a49275480e8f0639c62fc1f32af0dc385c960b7e
Vitaly _Vi Shukela <vi0oss@gmail.com>
2014-12-18 03:28:39 +0300

    Implement

--- loader_bpg.c
+++ loader_bpg.c
@@ -38,21 +38,115 @@
 #include <stdlib.h>
 #include <string.h>
 #include <fcntl.h>
+#include <stdio.h>
 
 #include <Imlib2.h>
 
 #include "imlib2_common.h"
 #include "loader.h"
 
+#include <inttypes.h>
+#include "libbpg.h"
 
 char load(ImlibImage * im, ImlibProgressFunction progress,
           char progress_granularity, char immediate_load)
 {
   
   int w,h;
+  uint8_t *buf = NULL;
+  int buf_len, buf_len_max;
+  FILE *f = NULL;
+  BPGImageInfo img_info_s, *img_info = &img_info_s;
+  BPGDecoderContext *img = NULL;
+  uint8_t *bgra = NULL;
+  int decoded_image_used = 0;
+  int y=0;
   
   if(im->data)
     return 0;
+
+
+  f = fopen(im->real_file, "rb");
+  if (!f) {
+        goto EXIT;
+  }
+
+  {
+    fseek(f, 0, SEEK_END);
+    buf_len_max = ftell(f);
+    fseek(f, 0, SEEK_SET);
+  }
+  if (buf_len_max < 1) {
+     buf_len_max = BPG_DECODER_INFO_BUF_SIZE;
+  }
+
+  buf = malloc(buf_len_max);
+  if (!buf) {
+		goto EXIT;
+  }
+  buf_len = fread(buf, 1, buf_len_max, f);
+
+  fclose(f); f = NULL;
+
+  
+  img = bpg_decoder_open();
+
+  if (bpg_decoder_decode(img, buf, buf_len) < 0) {
+		goto EXIT;
+  }
+  free(buf); buf = NULL;
+
+  
+  bpg_decoder_get_info(img, img_info);
+    
+  w = img_info->width;
+  h = img_info->height;
+
+  bgra = malloc(4 * w * h);
+  if (!bgra) goto EXIT;
+
+  bpg_decoder_start(img, BPG_OUTPUT_FORMAT_RGBA32);
+  for (y = 0; y < h; ++y) {
+     bpg_decoder_get_line(img, bgra + 4*w*y);
+
+	 int x;
+	 for (x=0; x < w; ++x) {
+		#define SWAP(a,b) { unsigned char tmp; tmp=a; a=b; b=tmp; }
+		SWAP(bgra[4*w*y + 4*x + 0], bgra[4*w*y + 4*x + 2]);
+		#undef SWAP
+	 }
+  }
+
+  bpg_decoder_close(img); img = NULL;
+  
+  
+  if(!im->loader && !im->data) {
+    im->w = w;
+    im->h = h;
+
+    if(!IMAGE_DIMENSIONS_OK(w, h))
+      goto EXIT;
+
+    SET_FLAGS(im->flags, F_HAS_ALPHA);
+    
+    im->format = strdup("bpg");
+  }
+
+  if((!im->data && im->loader) || immediate_load || progress) {
+    im->data = (DATA32*)bgra;
+    decoded_image_used = 1;
+    if(progress)
+      progress(im, 100, 0, 0, w, h);
+    return 1;
+  }
+
+  
+
+EXIT:
+  if (f) fclose(f);
+  if (buf) free(buf);
+  if (img) bpg_decoder_close(img);
+  if ((!decoded_image_used) && bgra) free(bgra);
 
   return 0;
 }
--- makefile
+++ makefile
@@ -1,8 +1,11 @@
 include commands.mk
 
+BPGDIR=/mnt/src/p/libbpg-0.9.3
+
 OPTS    := -O2
-CFLAGS  := -std=c99 $(OPTS) $(shell imlib2-config --cflags) -fPIC -Wall
-LDFLAGS := $(shell imlib2-config --libs) -lavcodec -lavformat -lswscale
+CFLAGS  := -std=c99 $(OPTS) $(shell imlib2-config --cflags) -fPIC -Wall -I${BPGDIR}
+LDFLAGS := $(shell imlib2-config --libs) -L${BPGDIR} -lbpg
+
 
 SRC = $(wildcard *.c)
 OBJ = $(foreach obj, $(SRC:.c=.o), $(notdir $(obj)))
@@ -32,10 +35,10 @@ clean:
 
 install:
 	$(INSTALL_DIR) $(DESTDIR)$(LOADERDIR)
-	$(INSTALL_LIB) ffmpeg.so $(DESTDIR)$(LOADERDIR)
+	$(INSTALL_LIB) bpg.so $(DESTDIR)$(LOADERDIR)
 
 uninstall:
-	$(RM) $(PLUGINDIR)/ffmpeg.so
+	$(RM) $(PLUGINDIR)/bpg.so
 
 -include $(DEP)
 
